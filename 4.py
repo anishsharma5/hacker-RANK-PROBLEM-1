55de7b7c5df120ef4680f45c32280fdd134fbc4b
anish sharma <105573375+xreddragonx@users.noreply.github.com>
2022-07-31 11:31:25 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,10 @@
+n=int(input())
+x=n%2
+if x!=0 :
+    print("Weird")
+if x==0 and n>=2 and n<=5 :
+    print("Not Weird")
+if x==0 and n>=6 and n<=20:
+    print("Weird")
+if n>20 and n%2==0:
+    print("Not Weird")
